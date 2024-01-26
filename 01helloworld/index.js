ceaa9317b83a26dc862a4f6d5a9148ab9ef04cec
Nikhil Giramkar <niksgiramkar@gmail.com>
2024-01-26 17:07:16 +0530

    Hello World

---
+++ 01helloworld/index.js
@@ -0,0 +1,11 @@
+console.log("Hello Nikhil");
+/*
+Console log is not just part of Javascript, its a part of V8 engine
+Due to which, we can not only run it on browser
+But also on server (due to NodeJs) or we can say terminal.
+
+To run this code, open terminal in VS Code
+Go to current directory where we kept this index.js
+write:
+    node index.js
+*/
