ea7da5ae4624b15e99b9f8b04fae9c0b8a0c58c5
Nikhil Giramkar <niksgiramkar@gmail.com>
2024-01-27 22:59:17 +0530

    Streams in Node Js

---
+++ 11_streams/index.js
@@ -0,0 +1,46 @@
+
+const http = require("http");
+
+const server = http.createServer();
+
+server.on("request", (req, res)=>{
+    //Traditional way
+    /*
+    Here, we fech the entire data source first,
+    once that is done, we show it on screen.
+    Problem occurs, when data set is very very huge.
+    Such that downloading/ fetching it taes significant amount of time.
+    In that case, the page keeps loading....
+    Until the content is completely downloaded
+    */
+    const fs = require("fs");
+    // fs.readFile("data.txt", "utf-8", (err, data)=>{
+    //     if(err)
+    //         return console.log(err);
+    //     res.end(data)
+    // }) 
+
+    //Streaming data
+    const readStream = fs.createReadStream("data.txt");
+
+    //When more chunks of data are yet to be streamed
+    readStream.on("data", (chunkData)=>{
+        res.write(chunkData);
+    })
+
+    //End of File
+    readStream.on("end", ()=>{
+        console.log("Document is completely streamed...");
+        res.end();
+    })
+
+    //Error in streaming
+    readStream.on("error", (err)=>{
+        console.log(err);
+        res.end("File Not found");
+    })
+})
+
+server.listen(8765, ()=>{
+    console.log("Server listening");
+})
